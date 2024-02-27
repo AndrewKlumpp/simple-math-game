7ca60f1c1962258450cc087621adfb85597830f4
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-02-27 01:47:52 -0700

    added progress bar and reset functionality

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const ourForm = document.querySelector(".our-form");
 const ourField = document.querySelector(".our-field");
 const pointsNeeded = document.querySelector(".points-needed");
 const mistakesAllowed = document.querySelector(".mistakes-allowed");
+const progressBar = document.querySelector(".progress-inner");
+const endMessage = document.querySelector(".end-message");
+const resetButton = document.querySelector(".reset-button");
 
 let state = {
     score: 0,
@@ -46,7 +49,8 @@ function handleSubmit(e) { // 'e' = event
     if (parseInt(ourField.value, 10) === correctAnswer) { // User submitted answer is correct
         state.score++;
         pointsNeeded.textContent = 10 - state.score;
-        updateProblem()
+        updateProblem();
+        renderProgressBar()
     } else { // User submitted answer is wrong
         state.wrongAnswers++;
         mistakesAllowed.textContent = 2 - state.wrongAnswers;
@@ -57,20 +61,30 @@ function handleSubmit(e) { // 'e' = event
 function checkLogic() {
     // if you win
     if(state.score === 10) {
-        alert("Congrats! You won.");
-        resetGame();
+        endMessage.textContent = "Congrats! You Won!";
+        document.body.classList.add("overlay-is-open");
+        setTimeout(() => resetButton.focus(), 331);
     };
     // if you lose
     if(state.wrongAnswers === 3) {
-        alert("Sorry, you lost.");
-        resetGame();
+        endMessage.textContent = "Sorry, you lost.";
+        document.body.classList.add("overlay-is-open");
+        setTimeout(() => resetButton.focus(), 331);
     };
 };
 
+resetButton.addEventListener("click", resetGame);
+
 function resetGame() {
+    document.body.classList.remove("overlay-is-open");
     updateProblem();
     state.score = 0;
     state.wrongAnswers = 0;
     pointsNeeded.textContent = 10;
     mistakesAllowed.textContent = 2;
+    renderProgressBar();
+};
+
+function renderProgressBar() {
+    progressBar.style.transform = `scaleX(${state.score / 10})`;
 };
